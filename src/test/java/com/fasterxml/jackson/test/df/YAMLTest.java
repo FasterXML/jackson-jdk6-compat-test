4b324628bd200ac8744b489e065b40e88effb272
Tatu Saloranta <tatu.saloranta@iki.fi>
2019-05-18 22:09:03 -0700

    fix yaml/cbor factories

--- src/test/java/com/fasterxml/jackson/test/df/YAMLTest.java
+++ src/test/java/com/fasterxml/jackson/test/df/YAMLTest.java
@@ -3,7 +3,7 @@ package com.fasterxml.jackson.test.df;
 import java.util.*;
 
 import com.fasterxml.jackson.databind.ObjectMapper;
-import com.fasterxml.jackson.dataformat.cbor.CBORFactory;
+import com.fasterxml.jackson.dataformat.yaml.YAMLFactory;
 import com.fasterxml.jackson.test.BaseTest;
 
 public class YAMLTest extends BaseTest
@@ -11,7 +11,7 @@ public class YAMLTest extends BaseTest
     @SuppressWarnings("serial")
     public void testSimple() throws Exception
     {
-        ObjectMapper mapper = newMapper(new CBORFactory());
+        ObjectMapper mapper = newMapper(new YAMLFactory());
         byte[] data = mapper.writeValueAsBytes(new HashMap<String,String>() { });
 
         Map<?,?> output = mapper.readValue(data, Map.class);
